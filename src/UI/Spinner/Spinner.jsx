53f69951cb4bf5ace492826e655c11a3f7928aae
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-11-11 18:04:25 +0200

    add module scss

--- src/UI/Spinner/Spinner.jsx
+++ src/UI/Spinner/Spinner.jsx
@@ -1,8 +1,12 @@
 import Loader from 'react-loader-spinner';
-import './Spinner.module.scss';
+import css from './Spinner.module.scss';
 
 const Spinner = () => {
-  return <Loader type="Rings" color="#4A56E2" height={42} width={42} />;
+  return (
+    <div className={css.Loader}>
+      <Loader type="Circles" color="#4a56e2" height={100} width={100} />
+    </div>
+  );
 };
 
 export default Spinner;
--- src/UI/spinner/spinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Spinner = () => {
-  return <div>spinner</div>;
-};
-
-export default Spinner;
