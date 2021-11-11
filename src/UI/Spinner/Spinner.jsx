d2e17bbe431f2798e36c569a70634632847edb44
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-11-11 16:17:34 +0100

    Update Spinner.jsx

--- src/UI/Spinner/Spinner.jsx
+++ src/UI/Spinner/Spinner.jsx
@@ -2,7 +2,7 @@ import Loader from 'react-loader-spinner';
 import './Spinner.module.scss';
 
 const Spinner = () => {
-  return <Loader type="Rings" color="#00BFFF" height={42} width={42} />;
+  return <Loader type="Rings" color="#4A56E2" height={42} width={42} />;
 };
 
 export default Spinner;
