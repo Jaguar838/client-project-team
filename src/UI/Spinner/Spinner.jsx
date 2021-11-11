f71cd00e29f393348cc2725c2716f3e51c2e61fe
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-11-11 17:23:10 +0200

    Merge pull request #4 from Jaguar838/dev
    Dev

---
+++ src/UI/Spinner/Spinner.jsx
@@ -0,0 +1,8 @@
+import Loader from 'react-loader-spinner';
+import './Spinner.module.scss';
+
+const Spinner = () => {
+  return <Loader type="Rings" color="#4A56E2" height={42} width={42} />;
+};
+
+export default Spinner;
