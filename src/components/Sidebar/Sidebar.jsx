f4e5f23d60ed999ba95db131ac211180fb1a7703
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-11-13 12:54:14 +0100

    creates Sidebar Navigation component

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import Media from 'react-media';
+import './Sidebar.scss';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      {/* <Navigation /> */}
+      {/* <Balance /> */}
+      <Media>{/* <Currency /> */}</Media>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './DashboardPage.scss';
 
-const Test = () => {
-  <div></div>;
+const DashboardPage = () => {
+  return (
+    <>
+      {/* <Header /> */}
+      <div className="dashboard">
+        <div className="main-container">
+          {/* <Sidebar /> */}
+          <main>
+            <Switch></Switch>
+          </main>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Test;
+export default DashboardPage;
