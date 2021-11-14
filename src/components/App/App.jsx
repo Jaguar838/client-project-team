0412efc0107f4a3320159199dbed415bfc157299
Oleksandr Melnyk <75173443+Alex-Melnik94@users.noreply.github.com>
2021-11-14 20:52:01 +0200

    Update App.jsx (#35)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,64 +11,69 @@
 //       </Route>
 //     </Container>
 
-// import { lazy, Suspense, useEffect } from 'react';
-// import { BrowserRouter, Switch } from 'react-router-dom';
-// import { store, persistor } from '../../redux/store';
+import { lazy, Suspense, useEffect } from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { store, persistor } from '../../redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-// import { Provider, useDispatch } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import Spinner from '../../UI/Spinner/';
-// import authOperations from '../../redux/auth/auth-operations';
+import authOperations from '../../redux/auth/auth-operations';
 import Container from '../Container';
-// import PrivateRoute from '../../routes/PrivateRouter';
-// import PublicRoute from '../../routes/PublicRouter';
+import PrivateRoute from '../../routes/PrivateRouter';
+import PublicRoute from '../../routes/PublicRouter';
 
-// const LoginPage = lazy(() =>
-//   import('../../pages/LoginPage' /* webpackChunkName: "LoginPage" */),
-// );
+const LoginPage = lazy(() =>
+  import('../../pages/LoginPage' /* webpackChunkName: "LoginPage" */),
+);
 
-// const RegistrationPage = lazy(() =>
-//   import(
-//     '../../pages/RegistrationPage' /* webpackChunkName: "RegistrationPage" */
-//   ),
-// );
+const RegistrationPage = lazy(() =>
+  import(
+    '../../pages/RegistrationPage' /* webpackChunkName: "RegistrationPage" */
+  ),
+);
 
-// const DashboardPage = lazy(() =>
-//   import(
-//     '../../pages/DashboardPage/DashboardPage' /* webpackChunkName: "DashboardPage" */
-//   ),
-// );
+const DashboardPage = lazy(() =>
+  import(
+    '../../pages/DashboardPage/DashboardPage' /* webpackChunkName: "DashboardPage" */
+  ),
+);
 
 function App() {
-  // const dispatch = useDispatch();
+  // const dispatch = useDispatch()
+  
+  // useEffect(() => {
+  //   dispatch(authOperations.refreshCurrentUser());
+  // }, [dispatch]);
+
 
   // useEffect(() => {
   //   dispatch(authOperations.refreshCurrentUser());
   // }, [dispatch]);
 
   return (
-    // <Provider store={store}>
-    // <PersistGate loading={null} persistor={persistor}>
-    //   <Container>
-    //     <BrowserRouter>
-    //       <Suspense fallback={<Spinner />} />
-    //       <Switch>
-    //         <PublicRoute path="/login" restricted>
-    //         <LoginPage />
-    //         </PublicRoute>
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Container>
+        <BrowserRouter>
+          <Suspense fallback={<Spinner />} />
+          <Switch>
+            <PublicRoute path="/login" restricted>
+            <LoginPage />
+            </PublicRoute>
 
-    //         <PublicRoute path="/register" restricted>
-    //         <RegistrationPage />
-    //         </PublicRoute>
+            <PublicRoute path="/register" restricted>
+            <RegistrationPage />
+            </PublicRoute>
 
-    //         <PrivateRoute path="/dashboard">
-    //         <DashboardPage />
-    //         </PrivateRoute>
-    //       </Switch>
-    //     </BrowserRouter>
-    //   </Container>
-    //   //{' '}
-    // </PersistGate>
-    // </Provider>
+            <PrivateRoute path="/dashboard">
+            <DashboardPage />
+            </PrivateRoute>
+          </Switch>
+        </BrowserRouter>
+      </Container>
+      //{' '}
+    </PersistGate>
+    </Provider>
     <Container>
       <Spinner />
     </Container>
