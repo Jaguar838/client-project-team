c50977910bc5f96a1903463879f80537f2949a96
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-11-14 18:59:02 +0200

    error react-redux

--- src/routes/PrivateRouter.js
+++ src/routes/PrivateRouter.js
@@ -1,12 +1,12 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router';
 import authSelectors from '../redux/auth/auth-selectors';
 
-export default function PrivateRoute({ children, ...routeProps }) {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  return (
-    <Route {...routeProps}>
-      {isLoggedIn ? children : <Redirect to="/login" />}
-    </Route>
-  );
-}
+// export default function PrivateRoute({ children, ...routeProps }) {
+//   // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+//   return (
+//     <Route {...routeProps}>
+//       {isLoggedIn ? children : <Redirect to="/login" />}
+//     </Route>
+//   );
+// }
