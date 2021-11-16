d9bca06fefa5cfd2b31b5c4a0844f0e4753cb93b
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-11-17 01:42:20 +0200

    add transaction redux

--- src/redux/transactions/transactions-operations.js
+++ src/redux/transactions/transactions-operations.js
@@ -1,3 +1,24 @@
-// import axios from 'axios';
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import { BASE_URL } from '../../assets/constants';
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { BASE_URL } from '../../assets/constants';
+axios.defaults.baseURL = BASE_URL;
+
+const setToken = token => {
+  if (!axios.defaults.headers.common.Authorization)
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const getTransactionOperation = createAsyncThunk(
+  'transactions/getFinance',
+  async (token, thunkAPI) => {
+    try {
+      setToken(token);
+      const { data } = await axios.get('transactions');
+      return data.data.transactions;
+    } catch (error) {
+      return thunkAPI.rejectWithValue();
+    }
+  },
+);
+
+export default getTransactionOperation;
