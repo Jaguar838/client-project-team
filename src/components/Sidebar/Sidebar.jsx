cbeaa933843d21da3fe701de62cf67bd04f258ff
Eugen Soliar <74872776+solarenergy990@users.noreply.github.com>
2021-11-16 14:23:48 +0100

    Bugfix/dashboard & sidebar & navigation (#46)
    * fixes styles in DashboardPage componet & in Sidebar component
    
    * activates Container in Dashboard component
    
    * erases styles that were put on root id
    
    * fixes Navigation component
    
    changes nav styles to moduled ones

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,24 @@
 import Media from 'react-media';
 import Navigation from './Navigation';
 import Currency from '../Currency';
-import './Sidebar.scss';
+import Balance from '../../UI/Balance';
+import style from './Sidebar.module.scss';
 
 const Sidebar = () => {
   return (
-    <aside className="sidebar">
+    <aside className={style.sidebar}>
       <Navigation />
-      {/* <Balance /> */}
+
+      <div className={style.balance}>
+        <Balance />
+      </div>
+
       <Media
         queries={{
           tablet: '(min-width: 768px)',
         }}
       >
         {({ tablet }) => <>{tablet && <Currency />}</>}
-        {/* <Currency /> */}
       </Media>
     </aside>
   );
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,28 +1,27 @@
 import { Route, Switch } from 'react-router-dom';
 import Sidebar from '../../components/Sidebar';
 import Currency from '../../components/Currency/Currency';
-// import Header from '../../UI/Header';
+import Header from '../../UI/Header';
 import TransactionTab from '../../components/TransactionTab';
-// import Container from '../../components/Container';
+import Container from '../../components/Container';
 import style from './DashboardPage.module.scss';
 
 const DashboardPage = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <div className={style.dashboard}>
-        {/* <Container> */}
-        <div className={style.container}>
-          <Sidebar />
-          <main>
-            <Switch>
-              <Route exact path="/home" component={TransactionTab} />
-
-              <Route exact path="/currency" component={Currency} />
-            </Switch>
-          </main>
-        </div>
-        {/* </Container> */}
+        <Container>
+          <div className={style.container}>
+            <Sidebar />
+            <main>
+              <Switch>
+                <Route exact path="/dashboard" component={TransactionTab} />
+                <Route exact path="/currency" component={Currency} />
+              </Switch>
+            </main>
+          </div>
+        </Container>
       </div>
     </>
   );
