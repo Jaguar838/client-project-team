7adee456bce2e8b5a6201509f49ed1289d5dd4c7
AlonaNihalchuk <acetnikopol@gmail.com>
2021-11-17 12:25:36 +0100

    next changes

--- src/components/Statistics/Chart/Chart.jsx
+++ src/components/Statistics/Chart/Chart.jsx
@@ -1,36 +1,64 @@
+// import { Doughnut } from 'react-chartjs-2';
+// import style from './Chart.module.scss';
+
+// // то как я вижу массив данных статистики с бэка
+// import { data } from '../Table/statistics.json';
+
+// const expensesCategory = data.filter(item => item.isExpense);
+// const balance = data[data.length - 1].balance;
+
+// const dataNut = {
+//   datasets: [
+//     {
+//       label: '# of Votes',
+//       // по типу примера из документации
+//       //   data: [132, 196, 113, 115, 112, 350, 70, 45, 99],
+
+//       // изменяя поле "amount" в statistics.json можно увидеть как соответственно изменяется диаграмма
+//       data: expensesCategory.map(item => {
+//         return item.amount;
+//       }),
+
+//       backgroundColor: expensesCategory.map(item => {
+//         return item.color;
+//       }),
+
+//       borderColor: expensesCategory.map(item => {
+//         return item.color;
+//       }),
+//       borderWidth: 1,
+//     },
+//     { width: 1 },
+//   ],
+// };
+
+// function withChartSizeControl(Component) {
+//   return props => (
+//     <div
+//       className={style.chart}
+//       style={{
+//         height: props.height + 'px',
+//         width: props.width + 'px',
+//       }}
+//     >
+//       <div className={style.balance}>₴ {balance}</div>
+//       <Component {...props} />
+//     </div>
+//   );
+// }
+
+// const NewDoughnut = withChartSizeControl(Doughnut);
+
+// // проп с data будет приходить сюда, подумать как прокинуть его в обьект настроек dataNat
+
+// const DoughnutChart = () => (
+//   <NewDoughnut className={style.newDoughnut} data={dataNut} />
+// );
+
+// export default DoughnutChart;
+
 import { Doughnut } from 'react-chartjs-2';
 import style from './Chart.module.scss';
-
-// то как я вижу массив данных статистики с бэка
-import { data } from '../Table/statistics.json';
-
-const expensesCategory = data.filter(item => item.isExpense);
-const balance = data[data.length - 1].balance;
-
-const dataNut = {
-  datasets: [
-    {
-      label: '# of Votes',
-      // по типу примера из документации
-      //   data: [132, 196, 113, 115, 112, 350, 70, 45, 99],
-
-      // изменяя поле "amount" в statistics.json можно увидеть как соответственно изменяется диаграмма
-      data: expensesCategory.map(item => {
-        return item.amount;
-      }),
-
-      backgroundColor: expensesCategory.map(item => {
-        return item.color;
-      }),
-
-      borderColor: expensesCategory.map(item => {
-        return item.color;
-      }),
-      borderWidth: 1,
-    },
-    { width: 1 },
-  ],
-};
 
 function withChartSizeControl(Component) {
   return props => (
@@ -41,18 +69,13 @@ function withChartSizeControl(Component) {
         width: props.width + 'px',
       }}
     >
-      <div className={style.balance}>₴ {balance}</div>
+      <div className={style.balance}>₴ 25.0000</div>
+
       <Component {...props} />
     </div>
   );
 }
 
-const NewDoughnut = withChartSizeControl(Doughnut);
+const Chart = withChartSizeControl(Doughnut);
 
-// проп с data будет приходить сюда, подумать как прокинуть его в обьект настроек dataNat
-
-const DoughnutChart = () => (
-  <NewDoughnut className={style.newDoughnut} data={dataNut} />
-);
-
-export default DoughnutChart;
+export default Chart;
--- src/components/Statistics/ParentStats.jsx
+++ src/components/Statistics/ParentStats.jsx
@@ -1,12 +1,53 @@
-import Doughnut from './Chart';
+// import Doughnut from './Chart';
+// import Table from './Table';
+
+// const ParentStats = () => {
+//   return (
+//     <>
+//       <Doughnut />
+//       <Table />
+//     </>
+//   );
+// };
+// export default ParentStats;
+
 import Table from './Table';
+import Chart from './Chart';
+import expenses from './Table/statistics.json';
+import styles from './styles.module.scss';
 
 const ParentStats = () => {
+  const data = {
+    datasets: [
+      {
+        label: '# of Votes',
+        // подумать как сюда передать сумму расходов по каждой из категорий
+        data: [132, 196, 113, 115, 112, 350, 70, 45, 99],
+        backgroundColor: expenses.map(item => {
+          return item.color;
+        }),
+        borderColor: expenses.map(item => {
+          return item.color;
+        }),
+        borderWidth: 1,
+      },
+      { width: 1 },
+    ],
+  };
+
   return (
-    <>
-      <Doughnut />
-      <Table />
-    </>
+    <div className={styles.statsContainer}>
+      <h2 className={styles.title}>Статистика</h2>
+      <div className={styles.statsWrapper}>
+        <div className={styles.chartTab}>
+          <Chart data={data} />
+        </div>
+        <div className={styles.categoriesTab}>
+          <Table />
+        </div>
+      </div>
+    </div>
   );
 };
+
 export default ParentStats;
