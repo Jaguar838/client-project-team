7ca5cb78c61ca0a54e3a841af4516847dc4ac46f
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-11-17 16:12:40 +0200

    add pages login and registration component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,20 +1,20 @@
-import SectionAuthWrap from "../UI/SectionAuthWrap";
-import ContainerAuthImage from "../UI/ContainerAuthImage";
-import ContainerAuthForm from "../UI/ContainerAuthForm";
-import SvgIcon from "../UI/SvgIcon";
-
+import SectionAuthWrap from '../UI/SectionAuthWrap';
+import ContainerAuthImage from '../UI/ContainerAuthImage';
+import ContainerAuthForm from '../UI/ContainerAuthForm';
+import SvgIcon from '../UI/SvgIcon';
+import LoginForm from '../components/LoginForm';
 const HomePage = () => {
-    return (
-        <SectionAuthWrap>
-            <ContainerAuthImage title={'Finance App'}>
-                <SvgIcon iconName={'loginPicture-tablet'}/>
-                <SvgIcon iconName={'loginPicture-desc'}/>
-            </ContainerAuthImage>
-            <ContainerAuthForm formContainer={'loginForm'}>
-                {/* <LoginForm /> */}
-            </ContainerAuthForm>
-        </SectionAuthWrap>
-    )
-}
+  return (
+    <SectionAuthWrap>
+      <ContainerAuthImage title={'Finance App'}>
+        <SvgIcon iconName={'loginPicture-tablet'} />
+        <SvgIcon iconName={'loginPicture-desc'} />
+      </ContainerAuthImage>
+      <ContainerAuthForm formContainer={'loginForm'}>
+        <LoginForm />
+      </ContainerAuthForm>
+    </SectionAuthWrap>
+  );
+};
 
 export default HomePage;
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,20 +1,21 @@
-import SectionAuthWrap from "../UI/SectionAuthWrap";
-import ContainerAuthImage from "../UI/ContainerAuthImage";
-import ContainerAuthForm from "../UI/ContainerAuthForm";
-import SvgIcon from "../UI/SvgIcon";
+import SectionAuthWrap from '../UI/SectionAuthWrap';
+import ContainerAuthImage from '../UI/ContainerAuthImage';
+import ContainerAuthForm from '../UI/ContainerAuthForm';
+import SvgIcon from '../UI/SvgIcon';
+import RegistrationForm from '../components/RegistrationForm';
 
 const HomePage = () => {
-    return (
-        <SectionAuthWrap>
-            <ContainerAuthImage title={'Finance App'}>
-                <SvgIcon iconName={'regPicture-tablet'}/>
-                <SvgIcon iconName={'regPicture-desc'}/>
-            </ContainerAuthImage>
-            <ContainerAuthForm formContainer={'registrationForm'}>
-                {/* <RegistrationForm /> */}
-            </ContainerAuthForm>
-        </SectionAuthWrap>
-    )
-}
+  return (
+    <SectionAuthWrap>
+      <ContainerAuthImage title={'Finance App'}>
+        <SvgIcon iconName={'regPicture-tablet'} />
+        <SvgIcon iconName={'regPicture-desc'} />
+      </ContainerAuthImage>
+      <ContainerAuthForm formContainer={'registrationForm'}>
+        <RegistrationForm />
+      </ContainerAuthForm>
+    </SectionAuthWrap>
+  );
+};
 
 export default HomePage;
