a36883e3d9763ca03f863a5d67b9663217570888
AlonaNihalchuk <acetnikopol@gmail.com>
2021-11-17 19:24:35 +0100

    add legend

--- src/components/Statistics/Chart/Chart.jsx
+++ src/components/Statistics/Chart/Chart.jsx
@@ -1,18 +1,12 @@
 import { Doughnut } from 'react-chartjs-2';
 import style from './Chart.module.scss';
 
-function withChartSizeControl(Component) {
+function withChartSizeControl(Doughnut) {
   return props => (
-    <div
-      className={style.chart}
-      style={{
-        height: props.height + 'px',
-        width: props.width + 'px',
-      }}
-    >
+    <div className={style.chart}>
       <div className={style.balance}>₴ 25.0000</div>
 
-      <Component {...props} />
+      <Doughnut {...props} />
     </div>
   );
 }
@@ -20,3 +14,99 @@ function withChartSizeControl(Component) {
 const Chart = withChartSizeControl(Doughnut);
 
 export default Chart;
+
+// import Media from 'react-media';
+// import { Doughnut } from 'react-chartjs-2';
+// import style from './Chart.module.scss';
+
+// import { data } from '../Table/statistics.json';
+
+// const expensesCategory = data.filter(item => item.isExpense);
+// // const balance = data[data.length - 1].balance;
+
+// const dataNut = {
+//   labels: expensesCategory.map(item => {
+//     return item.category;
+//   }),
+//   datasets: [
+//     {
+//       label: '# of Votes',
+//       data: expensesCategory.map(item => {
+//         return item.amount;
+//       }),
+
+//       backgroundColor: expensesCategory.map(item => {
+//         return item.color;
+//       }),
+
+//       borderColor: expensesCategory.map(item => {
+//         return item.color;
+//       }),
+//       borderWidth: 1,
+//     },
+//     { width: 1 },
+//   ],
+// };
+
+// const options = {
+//   plugins: {
+//     legend: {
+//       labels: {
+//         boxWidth: 20,
+//         boxHeight: 10,
+//         font: {
+//           size: 14,
+//         },
+//       },
+//       // display: false,
+//       position: 'top',
+//       align: 'start',
+//     },
+//   },
+// };
+// const secondOptions = {
+//   plugins: {
+//     legend: {
+//       labels: {
+//         boxWidth: 20,
+//         boxHeight: 10,
+//         font: {
+//           size: 14,
+//         },
+//       },
+//       display: false,
+//       position: 'top',
+//       align: 'start',
+//     },
+//   },
+// };
+// // maintainAspectRatio: false,
+// // usePointStyle: true,
+
+// const DoughnutChart = () => {
+//   return (
+//     <Media
+//       queries={{
+//         mobile: '(max-width: 767px)',
+//       }}
+//     >
+//       {({ mobile }) => (
+//         <>
+//           {mobile ? (
+//             <div className={style.chart}>
+//               <Doughnut data={dataNut} options={secondOptions} />
+//               <div className={style.balance}>₴ 25.0000</div>
+//             </div>
+//           ) : (
+//             <div className={style.chart}>
+//               <Doughnut data={dataNut} options={options} />
+//               <div className={style.balance}>₴ 25.0000</div>
+//             </div>
+//           )}
+//         </>
+//       )}
+//     </Media>
+//   );
+// };
+
+// export default DoughnutChart;
