fbf98bd89ab20645b92a0237c27d678775a40290
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-11-18 15:55:56 +0100

    refactors privateRouter component according to react-router v6

--- src/routes/PrivateRouter.js
+++ src/routes/PrivateRouter.js
@@ -1,13 +1,10 @@
 import { useSelector } from 'react-redux';
-import { Route, Redirect } from 'react-router';
+import { Navigate } from 'react-router';
+
 import authSelectors from '../redux/auth/auth-selectors';
 
 export default function PrivateRoute({ children, ...routeProps }) {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-    // const isLoggedIn = false
-  return (
-    <Route {...routeProps}>
-      {isLoggedIn ? children : <Redirect to="/login" />}
-    </Route>
-  );
+  // const isLoggedIn = true;
+  return isLoggedIn ? children : <Navigate to="/login" />;
 }
