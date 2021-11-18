c27865214b6e18d71915bf4ef0df2180a843faed
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-11-18 21:32:28 +0200

    Update Period.jsx

--- src/components/Statistics/Period/Period.jsx
+++ src/components/Statistics/Period/Period.jsx
@@ -1,71 +1,71 @@
-
 import { useEffect, useState } from 'react';
 import s from './Period.module.scss';
 
-const Period = (props) => {
-    
-  const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
-  //conts [date,setDate]=useState('');
+const Period = ({ setRequestedMonth, setRequestedYear, years }) => {
+  const [monthsState, setMonthState] = useState([]);
+  const [yearsState, setYearState] = useState([]);
 
-  const allMonth=['Месяц','Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь' ]
-  const allYears=['Год',2020,2021]
+  const allMonths = [
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь',
+  ];
+  const allYears = [2020, 2021]; //получить в пропах года и записать вместо массива
 
+  useEffect(() => {
+    const date = new Date();
+    setMonthState(date.getUTCMonth());
+    setYearState(date.getFullYear());
 
-  useEffect(()=>{
-    const date= new Date();
- setMonth(date.getUTCMonth());
- setYear(date.getFullYear())
+    // eslint-disable-next-line
+  }, []);
 
+  const validateMounth = e => {
+    const b = allMonths.findIndex(e.target.value);
+    setRequestedMonth(b + 1);
+  };
 
-   
-// eslint-disable-next-line  
-  },[])
+  const validateYears = e => {
+    setRequestedYear(e.target.value);
+  };
 
- 
+  return (
+    <>
+      <form className={s.form}>
+        <select
+          name="SelectedMounth"
+          className={s.select}
+          id="area"
+          onChange={validateMounth}
+        >
+          {allMonths.map(month => (
+            <option selected={allMonths.indexOf(month) === monthsState}>
+              {month}
+            </option>
+          ))}
+        </select>
 
-  
-  
-  const validateMounth=(e)=>{
-      
-      setMonth(e.target.value)
-      
-  }
-  const validateYears=(e)=>{
-    setYear(e.target.value)
-}
- const handelSubmit=()=>{
-    
-     props.onSubmit(month,year)
-
- }
-    return (
-      <>
-         <form onSubmit={handelSubmit} className={s.form}>
-              <select 
-              name="SelectedMounth"
-              className={s.select}
-  
-              id="area"
-              onChange={validateMounth}
-              >
-             {allMonth.map(value=>
-                 
-             <option >{value}</option>)}    
-              </select>
-              <select 
-              name="SelectedYears"
-              className={s.select}  
-              id="area"
-              onChange={validateYears}
-              >
-             {allYears.map(value=><option>{value}</option>)}    
-              </select>
-              
-              </form>
-      </>
-    );
-  
-  
+        <select
+          name="SelectedYears"
+          className={s.select}
+          id="area"
+          onChange={validateYears}
+        >
+          {allYears.map(year => (
+            <option selected={year === yearsState}>{year}</option>
+          ))}
+        </select>
+      </form>
+    </>
+  );
 };
 export default Period;
