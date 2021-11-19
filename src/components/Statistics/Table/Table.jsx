0bcd2fc957007f132cfeeff5eadf2fc46a07afb4
AlonaNihalchuk <74723056+AlonaNihalchuk@users.noreply.github.com>
2021-11-19 16:47:38 +0100

    bugFixStyles (#69)

--- src/components/Statistics/Table/Table.jsx
+++ src/components/Statistics/Table/Table.jsx
@@ -1,21 +1,18 @@
 import style from './Table.module.scss';
 
-function Table({data, expenses, incomes}) {
+function Table({ data, expenses, incomes }) {
   return (
     <div className={style.mainContainer}>
-      <table>
-        <thead className={style.head}>
-          <tr className={style.raw}>
-            <th className={style.tableHead}>Категория</th>
-            <th className={style.tableHead}>Сумма</th>
+      <table className={style.table}>
+        <thead>
+          <tr className={style.theadRaw}>
+            <th className={style.tableHead + ' ' + style.thLeft}>Категория</th>
+            <th className={style.tableHead + ' ' + style.thRight}>Сумма</th>
           </tr>
         </thead>
         <tbody>
           {data.map(({ name, amount, color, id }) => (
             <tr key={id} className={style.raw}>
-              {/* <td className={style.boxTd}>
-                <div style={{ background: color }} className={style.box}></div>
-              </td> */}
               <td className={style.commonContainer}>
                 {' '}
                 <div style={{ background: color }} className={style.box}></div>
@@ -26,11 +23,11 @@ function Table({data, expenses, incomes}) {
           ))}
         </tbody>
         <tfoot>
-          <tr className={style.raw}>
+          <tr className={style.raw + ' ' + style.lastRaw}>
             <td className={style.manyCategory}> Расходы:</td>
             <td className={style.expenses}>{expenses}</td>
           </tr>
-          <tr className={style.raw}>
+          <tr className={style.raw + ' ' + style.lastRaw}>
             <td className={style.manyCategory}>Доходы:</td>
             <td className={style.income}>{incomes}</td>
           </tr>
