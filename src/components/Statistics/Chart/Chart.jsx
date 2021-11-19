e6b5a9dc2c502434c5bdcc4b6d05fd69d29be3d1
AlonaNihalchuk <acetnikopol@gmail.com>
2021-11-19 15:43:51 +0100

    bugFixStyles

--- src/components/Statistics/Chart/Chart.jsx
+++ src/components/Statistics/Chart/Chart.jsx
@@ -1,33 +1,12 @@
-// import { Doughnut } from 'react-chartjs-2';
-// import style from './Chart.module.scss';
-
-// function withChartSizeControl(Doughnut) {
-//   return props => (
-//     <div className={style.chart}>
-//       <div className={style.balance}>₴ 25.0000</div>
-
-//       <Doughnut {...props} />
-//     </div>
-//   );
-// }
-
-// const Chart = withChartSizeControl(Doughnut);
-
-// export default Chart;
-
 import { Doughnut } from 'react-chartjs-2';
 import style from './Chart.module.scss';
 
-// maintainAspectRatio: false,
-// usePointStyle: true,
-
-const DoughnutChart = ({dataNut, options, balance}) => {
+const DoughnutChart = ({ dataNut, options, balance }) => {
   return (
-            <div className={style.chart}>
-              <Doughnut data={dataNut} options={options} />
+    <div className={style.chart}>
+      <Doughnut data={dataNut} options={options} />
       <div className={style.balance}>₴ {balance}</div>
-            </div>
-
+    </div>
   );
 };
 
