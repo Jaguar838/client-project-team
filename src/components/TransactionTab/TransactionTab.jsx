0a31aba0aed9b0a21296af403132617366d22d2d
Eugen Soliar <74872776+solarenergy990@users.noreply.github.com>
2021-11-19 17:19:31 +0100

    Bugfix/dashboard styles & routes (#70)
    * installs react-responsive, makes responsive routing
    
    installs react-responsive, makes responsive routing by adding media query
    
    * adds media query component
    
    added media query component to sidebar component
    
    * adds MediaQuery component to Navigation component
    
    added  MediaQuery component to Navigation component to return link by condition of actual screen width
    
    * fixes styles in dashboard page component & adds media query component to TransactionTab component
    
    * fixes styles in Sidebar component
    
    * Update Sidebar.module.scss
    
    * fixes styles in Container component

--- src/components/TransactionTab/TransactionTab.jsx
+++ src/components/TransactionTab/TransactionTab.jsx
@@ -1,24 +1,13 @@
-import Media from 'react-media';
 import HomeTab from './HomeTab';
 import MobileTab from './MobileTab';
-import { useMediaQuery } from '../../hooks/useMediaQuery';
-import { mediaBreakpoints } from '../../assets/constants';
+
+import MediaQuery from 'react-responsive';
 
 const TransactionTab = () => {
-  const minTablet = useMediaQuery(mediaBreakpoints.minTablet);
-  const maxMobile = useMediaQuery(mediaBreakpoints.maxMobile);
   return (
-    // <Media
-    //   queries={{
-    //     mobile: '(max-width: 767px)',
-    //   }}
-    // >
-    //   {({ mobile }) => <>{mobile ? <MobileTab /> : <HomeTab />}</>}
-    // </Media>
-    <>
-    { minTablet && <HomeTab />}
-    {maxMobile && <MobileTab />}
-    </>
+    <MediaQuery maxWidth={767}>
+      {matches => (matches ? <MobileTab /> : <HomeTab />)}
+    </MediaQuery>
   );
 };
 
