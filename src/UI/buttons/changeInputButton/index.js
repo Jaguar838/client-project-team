abcaea6a0e83a1401cf4320b459051e1f12ef14a
Kochetov_R <k04erg0@gmail.com>
2021-11-21 22:25:02 +0200

    bugfix user info popup

--- src/UI/buttons/changeInputButton/index.js
+++ src/UI/buttons/changeInputButton/index.js
@@ -1,11 +1,22 @@
 import css from './styles.module.scss';
+import PropTypes from "prop-types";
 
-const ChabgeInputButton = ({ onChange }) => {
+import pencil from "./pencil.png";
+
+const ChabgeInputButton = ({ onChange, pencil }) => {
   return (
     <button type="button" className={css.button} onClick={onChange}>
-      <img src="./pencil.png" alt="pencil" />
+      <img src={pencil} alt="pencil" />
     </button>
   );
+};
+
+ChabgeInputButton.defaultProps = {
+  pencil: pencil,
+};
+
+ChabgeInputButton.propTypes = {
+  pencil: PropTypes.string
 };
 
 export default ChabgeInputButton;
