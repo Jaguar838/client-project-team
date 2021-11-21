62d0b4c691af5b173245f93668253719126c8ee9
Dmitry Kishkanev <donbdimo@gmail.com>
2021-11-21 15:59:53 +0200

    bugfix/forms

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -10,6 +10,7 @@ import { Formik } from 'formik';
 import * as yup from 'yup';
 import registerIcon from '../../assets/img/registerIcon.svg';
 import PasswordStrengthMete from './PasswordStrengthMeter';
+import LogoutButton from '../../UI/buttons/LogoutButton'
 
 const RegistrationForm = () => {
   const [password, setPassword] = useState('');
