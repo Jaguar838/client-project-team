4d0c260939e0d7c7d9c6e8bf607e8080b873799b
KochetovR <74375624+KochetovR@users.noreply.github.com>
2021-11-21 22:39:29 +0200

    bugfix user info popup (#99)

--- src/UI/Header/UserInfoPopup/UserInfoPopup.jsx
+++ src/UI/Header/UserInfoPopup/UserInfoPopup.jsx
@@ -4,18 +4,25 @@ import { useSelector } from 'react-redux';
 import authSelectors from '../../../redux/auth/auth-selectors';
 import ChabgeInputButton from '../../buttons/changeInputButton';
 import AvatarUploader from '../../../components/AvatarUploader';
-
+import authOperations from '../../../redux/auth/auth-operations';
 import PropTypes from "prop-types";
-
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 import css from './UserInfoPopup.module.scss'
 import defaultAvatar from "../UserMenu/user.png";
 
 const modalRoot = document.querySelector('#root-modal');
 
-const UserInfoPopup = ({ onClose, userAvatar }) => {
+const UserInfoPopup = ({ onClose, avatar }) => {
+  // const userAvatar = useSelector(state => authSelectors.getUserAvatar(state));
     // const userName = useSelector(state => authSelectors.getUsername(state));
+    // const userEmail = useSelector(state => authSelectors.getUserEmail(state));
+
+    // const dispatch = useDispatch();
+
     const windowListener = useRef(null);
 
     const [userName, setUserName] = useState('Roman');
@@ -23,6 +30,11 @@ const UserInfoPopup = ({ onClose, userAvatar }) => {
 
     useEffect(() => {
       windowListener.current = window.addEventListener('keydown', handleKeyDown);
+    });
+
+    const validationSchema = yup.object().shape({
+      userEmail: yup.string().email('Введите верный email').required('Обязательно'),
+      userName: yup.string().typeError('Должно быть строкой').required('Обязательно'),
     });
 
     const handleKeyDown = e => {
@@ -45,11 +57,11 @@ const UserInfoPopup = ({ onClose, userAvatar }) => {
         const { name, value } = e.target;
     
         switch (name) {
-          case 'userName':
+          case 'name':
             setUserName(value);
             break;
     
-          case 'userEmail':
+          case 'email':
             setUserEmail(value);
             break;
     
@@ -58,21 +70,33 @@ const UserInfoPopup = ({ onClose, userAvatar }) => {
         }
       };
 
+      const handleSubmit = e => {
+        e.preventDefault();
+        // dispatch(authOperations.editUserInfo({ userName, userEmail}));
+      };
+
     return createPortal(
         <div className={css.popupBackdrop} onClick={handleBackdropClick}>
             <div className={css.popupContent}>
+                <div className={css.topBlock}></div>
                 <div className={css.avatarBlock}>
-                <img className={css.userAvatar} src={userAvatar} alt={userName}/>  
-                <button type='button' className={css.changeAvatarButton}></button>
+                {/* {userAvatar ?
+                  <img className={css.userAvatar} src={userAvatar} alt={userName}/> 
+                  :
+                  <img className={css.userAvatar} src={avatar} alt={userName}/> 
+                } */}
+                <img className={css.userAvatar} src={avatar} alt={userName}/>
                 </div>
-                <form className={css.userForm}>
+                <form className={css.userForm} onSubmit={handleSubmit}>
                     <div className={css.userInfoLine}>
                         <input
                         className={css.userInput}
                         type={`text`}
                         value={userName}
-                        placeholder={userName}
+                        name="name"
                         onChange={handleInputChange}
+                        validationSchema={validationSchema}
+                        // pattern={validationSchema}
                         />
                         <ChabgeInputButton />
                     </div>
@@ -82,8 +106,9 @@ const UserInfoPopup = ({ onClose, userAvatar }) => {
                         className={css.userInput}
                         type={`text`}
                         value={userEmail}
-                        placeholder={userEmail}
+                        name="email"
                         onChange={handleInputChange}
+                        // pattern={validationSchema}
                         />
                         <ChabgeInputButton />
                     </div>
@@ -92,8 +117,8 @@ const UserInfoPopup = ({ onClose, userAvatar }) => {
                         <button className={css.buttonSubmit}>Применить</button>
                         <button onClick={onClose} className={css.buttonCancel}>Отменить</button>
                     </div>
-          </form>
-          <AvatarUploader/>
+                </form>
+                <AvatarUploader/>
             </div>
         </div>,
         modalRoot,
@@ -101,13 +126,13 @@ const UserInfoPopup = ({ onClose, userAvatar }) => {
 }
 
 UserInfoPopup.defaultProps = {
-    userAvatar: defaultAvatar,
+    avatar: defaultAvatar,
     userName: "Имя",
     userEmail: 'email'
   };
   
   UserInfoPopup.propTypes = {
-    userAvatar: PropTypes.string,
+    avatar: PropTypes.string,
     userName: PropTypes.string.isRequired,
     userEmail: PropTypes.string.isRequired,
   };
