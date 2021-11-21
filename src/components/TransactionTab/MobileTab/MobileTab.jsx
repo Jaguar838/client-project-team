699980880830ca9872b4963d2ff527c16542174e
Andrei Lysenkovskiy <75451047+andreilysen@users.noreply.github.com>
2021-11-21 19:39:32 +0200

    fix mobile style (#92)

--- src/components/TransactionTab/MobileTab/MobileTab.jsx
+++ src/components/TransactionTab/MobileTab/MobileTab.jsx
@@ -1,30 +1,26 @@
 import { useSelector } from 'react-redux';
 import {
   getAllTransactions,
-  getLoader,
+  // getLoader,
 } from '../../../redux/transactions/transactions-selectors';
 import styles from './MobileTab.module.scss';
 
 const MobileTab = () => {
   const transactions = useSelector(state => getAllTransactions(state));
-  const isLoading = useSelector(state => getLoader(state));
+  // const isLoading = useSelector(state => getLoader(state));
   let filterTrans = [...transactions];
   filterTrans.sort((a, b) => (a.date > b.date ? -1 : 1));
 
-  return isLoading ? (
-    <div>
-      <span> Loading... </span>
-    </div>
-  ) : (
+  return (
     <div className={styles.container}>
       {filterTrans?.map(item => {
-        const checked = () => {
-          if (!item.category) {
-            return 'sorry';
-          }
+        // const checked = () => {
+        //   if (!item.category) {
+        //     return 'sorry';
+        //   }
 
-          return item.category.name;
-        };
+        //   return item.category.name;
+        // };
         const border = item.isExpense ? '#ff6596' : '#24cca7';
         const text = item.isExpense ? '-' : '+';
         const colorTxt = item.isExpense ? styles.lose : styles.profit;
@@ -45,7 +41,7 @@ const MobileTab = () => {
               </tr>
               <tr>
                 <th>Категория</th>
-                <td>{checked()}</td>
+                <td>{item.category.name}</td>
               </tr>
               <tr>
                 <th>Комментарий</th>
