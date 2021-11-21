ed4d27dca0e15b700b888ac17520172f081793e4
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-11-21 17:57:42 +0200

    fix mobile style

--- src/components/TransactionTab/HomeTab/HomeTab.jsx
+++ src/components/TransactionTab/HomeTab/HomeTab.jsx
@@ -1,14 +1,14 @@
 import { useSelector } from 'react-redux';
 import {
   getAllTransactions,
-  getLoader,
+  // getLoader,
 } from '../../../redux/transactions/transactions-selectors';
 
 import styles from './HomeTab.module.scss';
 
 const HomeTab = () => {
   const transactions = useSelector(state => getAllTransactions(state));
-  const isLoading = useSelector(state => getLoader(state));
+  // const isLoading = useSelector(state => getLoader(state));
   let filterTrans = [...transactions];
   filterTrans.sort((a, b) => (a.date > b.date ? -1 : 1));
   return (
@@ -25,37 +25,31 @@ const HomeTab = () => {
           </tr>
         </thead>
         <tbody className={styles.tbody}>
-          {isLoading ? (
-            <tr>
-              <td> Loading... </td>
-            </tr>
-          ) : (
-            filterTrans?.map(item => {
-              const checked = () => {
-                if (!item.category) {
-                  return 'sorry';
-                }
+          {filterTrans?.map(item => {
+            // const checked = () => {
+            //   if (!item.category) {
+            //     return 'sorry';
+            //   }
 
-                return item.category.name;
-              };
+            //   return item.category.name;
+            // };
 
-              const text = item.isExpense ? '-' : '+';
-              const colorTxt = item.isExpense ? styles.lose : styles.profit;
+            const text = item.isExpense ? '-' : '+';
+            const colorTxt = item.isExpense ? styles.lose : styles.profit;
 
-              // const date = new Date(item.date);
-              // const parsDate = date.toLocaleDateString();
-              return (
-                <tr key={item.id} className={styles.tr}>
-                  <td>{item.date_str}</td>
-                  <td>{text}</td>
-                  <td>{checked()}</td>
-                  <td>{item.comment}</td>
-                  <td className={colorTxt}>{item.amount}</td>
-                  <td>{item.balanceAfter}</td>
-                </tr>
-              );
-            })
-          )}
+            // const date = new Date(item.date);
+            // const parsDate = date.toLocaleDateString();
+            return (
+              <tr key={item.id} className={styles.tr}>
+                <td>{item.date_str}</td>
+                <td>{text}</td>
+                <td>{item.category.name}</td>
+                <td>{item.comment}</td>
+                <td className={colorTxt}>{item.amount}</td>
+                <td>{item.balanceAfter}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
