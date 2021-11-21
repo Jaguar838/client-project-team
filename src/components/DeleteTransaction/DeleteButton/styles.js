c061400683eaa22187a0e93d69bc605a501c44eb
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-11-21 19:11:04 +0200

    opacity for button

--- src/components/DeleteTransaction/DeleteButton/styles.js
+++ src/components/DeleteTransaction/DeleteButton/styles.js
@@ -4,9 +4,11 @@ import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 export const BtnDelete = styled(DeleteForeverIcon)`
   cursor: pointer;
   color: #ff6596;
-  transition: 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  opacity: 0.5;
+  transition: opacity 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94), transform 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
 
   &:hover {
+    opacity: 1;
     transform: scale(1.2);
   }
 `;
--- src/components/EditTransaction/EditButton/styles.js
+++ src/components/EditTransaction/EditButton/styles.js
@@ -4,9 +4,11 @@ import EditIcon from '@material-ui/icons/Edit';
 export const BtnEdit = styled(EditIcon)`
   color: #24cca7;
   cursor: pointer;
-  transition: 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  opacity: 0.5;
+  transition: opacity 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94), transform 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
 
   &:hover {
+    opacity: 1;
     transform: scale(1.2)
   }
 `;
